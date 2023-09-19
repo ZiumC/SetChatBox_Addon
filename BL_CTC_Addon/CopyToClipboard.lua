4eed8a7cc42c670a4dc3a77a50f3d5d47b9bc0d2
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-09-19 19:26:31 +0200

    Initial commit, added button to dropdown menu

---
+++ BL_CTC_Addon/CopyToClipboard.lua
@@ -0,0 +1,22 @@
+CTC_PLAYER_NAME_BTN = "COPY_PLAYER_NAME"
+CTC_PLAYER_NAME_PROMPT = "Copy name"
+
+local DropdownMenuList = {"PLAYER","RAID_PLAYER","PARTY","TARGET","FRIEND",}
+
+local function menuButtonFunction(self)	
+	if self.value == CTC_PLAYER_NAME_BTN then
+		print("Target added to list")		
+	end
+end
+
+UnitPopupButtons[CTC_PLAYER_NAME_BTN] = {
+	text = CTC_PLAYER_NAME_PROMPT,
+	dist = 0,
+	icon = "Interface\\TargetingFrame\\UI-RaidTargetingIcon_1.png",
+}
+
+for k,v in pairs(DropdownMenuList) do		
+	table.insert(UnitPopupMenus[v],CTC_PLAYER_NAME_BTN)
+end
+
+hooksecurefunc("UnitPopup_OnClick",menuButtonFunction)
