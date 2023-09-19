839c1ec572d5861c5bde3bf82c3adc028e90cc3a
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-09-19 19:45:00 +0200

    Added second button to copy

--- BL_CTC_Addon/CopyToClipboard.lua
+++ BL_CTC_Addon/CopyToClipboard.lua
@@ -1,5 +1,8 @@
 CTC_PLAYER_NAME_BTN = "COPY_PLAYER_NAME"
-CTC_PLAYER_NAME_PROMPT = "Copy"
+CTC_PLAYER_NAME_PROMPT = "Copy Name"
+
+CTC_PLAYER_NAME_WITH_COMMAND_BTN = "COPY_PLAYER_NAME_WITH_COMMAND"
+CTC_PLAYER_NAME_WITH_COMMAND_PROMPT = "Copy & Command"
 
 local DropdownMenuList = {"PLAYER","RAID_PLAYER","PARTY","TARGET","FRIEND",}
 
@@ -7,16 +10,29 @@ local function menuButtonFunction(self)
 	if self.value == CTC_PLAYER_NAME_BTN then
 		print("Target added to list")		
 	end
+    if self.value == CTC_PLAYER_NAME_WITH_COMMAND_BTN then
+		print("Target added to list")		
+	end
 end
 
 UnitPopupButtons[CTC_PLAYER_NAME_BTN] = {
-	text = CTC_PLAYER_NAME_PROMPT,
+    text = CTC_PLAYER_NAME_PROMPT,
+	dist = 0,
+	icon = "Interface\\TargetingFrame\\UI-RaidTargetingIcon_3.png",
+}
+
+UnitPopupButtons[CTC_PLAYER_NAME_WITH_COMMAND_BTN] = {
+    text = CTC_PLAYER_NAME_WITH_COMMAND_PROMPT,
 	dist = 0,
 	icon = "Interface\\TargetingFrame\\UI-RaidTargetingIcon_1.png",
 }
 
 for k,v in pairs(DropdownMenuList) do		
 	table.insert(UnitPopupMenus[v],CTC_PLAYER_NAME_BTN)
+end
+
+for k,v in pairs(DropdownMenuList) do		
+	table.insert(UnitPopupMenus[v],CTC_PLAYER_NAME_WITH_COMMAND_BTN)
 end
 
 hooksecurefunc("UnitPopup_OnClick",menuButtonFunction)
