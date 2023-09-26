38d06df979618a4910bb3fdec017c5c14115ae1c
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-09-26 12:41:11 +0200

    Added function that return length of obj and also added options next to the player name

--- DisplayInChatBox/RaidMenu.lua
+++ DisplayInChatBox/RaidMenu.lua
@@ -1,15 +1,41 @@
 local raidPlayersCount = GetNumGroupMembers()
 
+BUTTONS = {
+    {
+        title = "Append",
+        icon = "Interface\\TargetingFrame\\UI-RaidTargetingIcon_4.png"
+    },
+    {
+        title = "!check",
+        icon = "Interface\\TargetingFrame\\UI-RaidTargetingIcon_1.png"
+    },
+    {
+        title = "Name",
+        icon = "Interface\\TargetingFrame\\UI-RaidTargetingIcon_3.png"
+    }
+}
+
+local function length(T)
+    local count = 0
+    for _ in pairs(T) do 
+        count = count + 1 
+    end
+    return count
+end
+
+-- create and configure dropdown menu
 local dropDown = CreateFrame("FRAME", "RaidDropDown", UIParent, "UIDropDownMenuTemplate")
 dropDown:SetPoint("TOP")
 UIDropDownMenu_SetWidth(dropDown, 200)
 UIDropDownMenu_SetText(dropDown, "Raid Players (" .. raidPlayersCount .. ")")
 
+-- create and bind the function to dropdown menu
 UIDropDownMenu_Initialize(dropDown, function(self, level, menuList)
     local info = UIDropDownMenu_CreateInfo()
 
+    raidPlayersCount = GetNumGroupMembers()
     UIDropDownMenu_SetText(dropDown, "Raid Players (" .. raidPlayersCount .. ")")
-    
+
     if (level or 1) == 1 then
         -- display raid players
         for i = 1, 40 do
@@ -20,4 +46,13 @@ UIDropDownMenu_Initialize(dropDown, function(self, level, menuList)
                 UIDropDownMenu_AddButton(info)
             end
         end
+
+    else
+        -- display a nested options of player
+        info.func = self.SetValue
+        for i = 1, length(BUTTONS) do
+            info.text, info.icon = BUTTONS[i].title, BUTTONS[i].icon
+            UIDropDownMenu_AddButton(info, level)
+        end 
+    end
 end)
