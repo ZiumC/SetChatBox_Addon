4e012f77e1fbb3be17ec6c4450bde05262b7c17e
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-09-26 11:35:48 +0200

    Added info how many players are in raid group

--- DisplayInChatBox/RaidMenu.lua
+++ DisplayInChatBox/RaidMenu.lua
@@ -1,4 +1,4 @@
-local raidPlayersCount = 0
+local raidPlayersCount = GetNumGroupMembers()
 
 local dropDown = CreateFrame("FRAME", "RaidDropDown", UIParent, "UIDropDownMenuTemplate")
 dropDown:SetPoint("TOP")
@@ -8,4 +8,16 @@ UIDropDownMenu_SetText(dropDown, "Raid Players (" .. raidPlayersCount .. ")")
 UIDropDownMenu_Initialize(dropDown, function(self, level, menuList)
     local info = UIDropDownMenu_CreateInfo()
 
+    UIDropDownMenu_SetText(dropDown, "Raid Players (" .. raidPlayersCount .. ")")
+    
+    if (level or 1) == 1 then
+        -- display raid players
+        for i = 1, 40 do
+            local name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML = GetRaidRosterInfo(i)
+            if name ~= nil then
+                info.text = name
+                info.menuList, info.hasArrow = i, true
+                UIDropDownMenu_AddButton(info)
+            end
+        end
 end)
