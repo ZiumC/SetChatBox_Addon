fe4715efe951d905e17edede641ef538f5de31ca
Rafał <90453529+ZiumC@users.noreply.github.com>
2023-09-26 19:56:52 +0200

    added label to frame

--- DisplayInChatBox/RaidMenu.lua
+++ DisplayInChatBox/RaidMenu.lua
@@ -24,6 +24,48 @@ local function length(T)
     return count
 end
 
+local function GetColorByClass(className)
+    if className == "Druid" then
+        return "cFFFFA500"
+    end
+
+    if className == "Paladin" then
+        return "cFFFFC0CB"
+    end
+
+    if className == "Death Knight" then
+        return "cFFFF0000"
+    end
+
+    if className == "Hunter" then
+        return "cFF90EE90"
+    end
+
+    if className == "Mage" then
+        return "cFFADD8E6"
+    end
+
+    if className == "Priest" then
+        return "cffffffff"
+    end
+
+    if className == "Rogue" then
+        return "cffffff00"
+    end
+
+    if className == "Shaman" then
+        return "cff0000ff"
+    end
+
+    if className == "Warlock" then
+        return "cFF800080"
+    end
+
+    if className == "Warrior" then
+        return "cFFD2B48C"
+    end
+end
+
 -- create and configure dropdown menu
 local dropDown = CreateFrame("FRAME", "RaidDropDown", UIParent, "UIDropDownMenuTemplate")
 dropDown:EnableMouse(true)
@@ -48,23 +90,24 @@ UIDropDownMenu_Initialize(dropDown, function(self, level, menuList)
         for i = 1, maxRaidPlayers do
             local name, rank, subgroup, class, fileName, zone, online, isDead, role, isML = GetRaidRosterInfo(i)
             if name ~= nil then
-                info.text = name
+                info.text = "\124" .. GetColorByClass(fileName) .. name .. "\124r"
                 info.func = self.ShowOptionWindow
                 info.arg1 = name
+                info.arg2 = fileName
                 UIDropDownMenu_AddButton(info)
             end
         end
     end
 end)
 
-function dropDown:ShowOptionWindow(playerName)
+function dropDown:ShowOptionWindow(playerName, className)
     UIDropDownMenu_SetText(dropDown, "Selected player: " .. playerName)
     CloseDropDownMenus()
 
+    -- create and configure window option
     local window = CreateFrame("FRAME", "PlayerOptionsFrame", UIParent)
-    window:SetSize(350, 100)
+    window:SetSize(150, 170)
     window:SetPoint("CENTER")
-
     window:SetBackdrop({
         bgFile = "Interface\\ChatFrame\\ChatFrameBackground",
         edgeFile = "Interface\\ChatFrame\\ChatFrameBackground",
@@ -78,7 +121,12 @@ function dropDown:ShowOptionWindow(playerName)
     window:SetScript("OnDragStart", window.StartMoving)
     window:SetScript("OnDragStop", window.StopMovingOrSizing)
     window:SetScript("OnHide", window.StopMovingOrSizing)
+    
+    window.text = window:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+    window.text:SetPoint("CENTER", window, "CENTER", 0, 45)
+    window.text:SetText("Actions for \124" .. GetColorByClass(className)  .. playerName .. "\124r")
 
+    -- create and add close btn
     local close = CreateFrame("BUTTON", "CloseBtn", window, "UIPanelCloseButton")
     close:SetPoint("TOPRIGHT", window, "TOPRIGHT")
     close:SetScript("OnClick", function()
@@ -86,32 +134,32 @@ function dropDown:ShowOptionWindow(playerName)
     end)
 
     local appendBtn = CreateFrame("BUTTON", "AppendBtn", window, "UIPanelButtonTemplate")
-	appendBtn:SetPoint("TOP", window, "CENTER", 0, 35)
+	appendBtn:SetPoint("TOP", window, "CENTER", 0, 20)
 	appendBtn:SetWidth(75)
 	appendBtn:SetHeight(25)
     appendBtn:SetText("Append")
     appendBtn:SetScript("OnClick", function()
-        print("a1")
+        print(playerName)
         window:Hide()
     end)
 	
     local checkBtn = CreateFrame("BUTTON", "CheckBtn", window, "UIPanelButtonTemplate")
-	checkBtn:SetPoint("TOP", window, "CENTER", 0, 0)
+	checkBtn:SetPoint("TOP", window, "CENTER", 0, -15)
 	checkBtn:SetWidth(75)
 	checkBtn:SetHeight(25)
     checkBtn:SetText("!check")
     checkBtn:SetScript("OnClick", function()
-        print("a2")
+        print(playerName)
         window:Hide()
     end)
 
     local nameBtn = CreateFrame("BUTTON", "CheckBtn", window, "UIPanelButtonTemplate")
-	nameBtn:SetPoint("TOP", window, "CENTER", 0,-35)
+	nameBtn:SetPoint("TOP", window, "CENTER", 0,-50)
 	nameBtn:SetWidth(75)
 	nameBtn:SetHeight(25)
     nameBtn:SetText("Name")
     nameBtn:SetScript("OnClick", function()
-        print("a3")
+        print("dupa")
         window:Hide()
     end)
 
